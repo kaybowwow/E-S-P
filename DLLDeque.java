b5ddc1fac89490cbc98faa3ba3cc6214f27560e7
JackieW00 <jwoo@stuy.edu>
2017-04-01 12:57:53 -0400

    added removeFirst and getFirst

--- DLLDeque.java
+++ DLLDeque.java
@@ -1,46 +1,101 @@
-public class DLLDeque<T> implements Deque {
+public class DLLDeque<T> implements Deque<T> {
     
     private DLLNode<T> _first , _last;
     private int _size;
 
-    public DLLDeque<T> () {
-	_first = null;
-	_last = null;
-	_size = 0;
+    public DLLDeque() {
+		_first = null;
+		_last = null;
+		_size = 0;
     } 
-
+    
+    /*---------------------------------------
+    precond: DLLDeque is created
+    postcond: adds DLLNode to front of DLLDeque
+    ---------------------------------------*/
     public void addFirst(T value) {
-	DLLNode<T> newNode = new DLLNode<T>(value , null , null); 
-	if (_size == 0) {
-	    _first = newNode;
-	    _end = newNode;
-	}
-	else {
-	    newNode.setNext(_first);
-	    _first = newNode;
-	}
-	_size += 1;
+		DLLNode<T> newNode = new DLLNode<T>(value , null , null); 
+		if (isEmpty()) {
+			_first = newNode;
+			_last = newNode;
+		}
+		else {
+			newNode.setNext(_first);
+			_first = newNode;
+		}
+		_size++;
+    }
+    
+    /*---------------------------------------
+    precond: DLLDeque is created
+    postcond: removes first element in DLLDeque
+    		  if DLLDeque is empty, return null
+    ---------------------------------------*/
+    public T removeFirst(){
+    	T ret = null;
+    	
+    	if (!isEmpty()){
+    		ret = _first.getCargo();
+    		_first = _first.getNext();
+    		_size--;
+    	}
+
+    	return ret;
+    }
+    
+    /*---------------------------------------
+    precond: DLLDeque is created
+    postcond: Returns first element in DLLDeque
+    ---------------------------------------*/
+    public T getFirst(){
+    	if (isEmpty()){
+    		return null;
+    	}
+    	return _first.getCargo();
+    }
+    
+    // Checks if DLLDeque is empty
+    public boolean isEmpty(){
+    	return _size == 0;
     }
 
-    /*
     // print each node, separated by spaces
     public String toString() 
     { 
-	String foo = "";
-	LLNode<T> tmp = _first;
-	while ( tmp != null ) {
-	    foo += tmp.getValue() + " ";
-	    tmp = tmp.getNext();
+		String foo = "";
+		DLLNode<T> tmp = _first;
+		while ( tmp != null ) {
+			foo += tmp.getCargo() + " ";
+			tmp = tmp.getNext();
+		}
+		return foo;
 	}
-	return foo;
-    }//O(n)
-    */
+    
     public static void main(String[] args) {
-	//DLLDeque<String> deeznuts = new DLLDeque<String>();
+		DLLDeque<String> deeznuts = new DLLDeque<String>();
 	
-	//deeznuts.addFirst("banana");
-
-	//System.out.println(deeznuts);
+		deeznuts.addFirst("banana");
+		deeznuts.addFirst("pear");
+		deeznuts.addFirst("cherry");
+		deeznuts.addFirst("lychee");
+		System.out.println("DLLDeque: " + deeznuts);
+		
+		
+		
+		System.out.println("removed: " + deeznuts.removeFirst()); // lychee
+		System.out.println("DLLDeque: " + deeznuts);
+		/*------------------ test removeFirst ---------------------
+		System.out.println("removed: " + deeznuts.removeFirst()); // cherry
+		System.out.println("DLLDeque: " + deeznuts);
+		System.out.println("removed: " + deeznuts.removeFirst()); // pear
+		System.out.println("DLLDeque: " + deeznuts);
+		System.out.println("removed: " + deeznuts.removeFirst()); // banana
+		System.out.println("DLLDeque: " + deeznuts);
+		System.out.println("removed: " + deeznuts.removeFirst()); // null
+		System.out.println("DLLDeque: " + deeznuts);
+		 ----------------------------------------------------------*/
+		
+		System.out.println("getFirst: " + deeznuts.getFirst()); //cherry
     }
 	    
 	  
--- Deque.java
+++ Deque.java
@@ -1,11 +1,13 @@
 public interface Deque<T> {
+	
+	public boolean isEmpty();
+	
     public void addFirst(T value);
-    /*---------------------------
 
     public T removeFirst();
 
     public T getFirst();
-
+    /*---------------------------
     public void addLast(T value);
 
     public T removeLast();
