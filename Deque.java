fb1f1413911189b4135d0165c4893a0345a47280
Max Chan <m2000chan@Maxs-MacBook-Pro-2.local>
2017-04-03 08:23:30 -0400

    Commented Deque

--- Deque.java
+++ Deque.java
@@ -1,16 +1,16 @@
 public interface Deque<T> {
 	
-    public boolean isEmpty();
+    public boolean isEmpty(); //returns boolean for whether Queue is empty
 	
-    public void addFirst(T value);
+    public void addFirst(T value);//Adds T value to the beginning of Queue
 
-    public T removeFirst();
+    public T removeFirst();//Removes first element from beginning of Queue, returns removed value
 
-    public T getFirst();
+    public T getFirst();//Returns first element without removing
 
-    public void addLast(T value);
+    public void addLast(T value);//Adds T value to the end of Queue
 
-    public T removeLast();
+    public T removeLast();//Removes last element from end of Queue, returns removed value
 
-    public T getLast();
+    public T getLast();//Returns last element without removing
 }
