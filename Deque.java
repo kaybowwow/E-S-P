f07b65c26f9296230a6b37dea00ac7a82463930d
Max Chan <m2000chan@Maxs-MacBook-Pro-2.local>
2017-04-03 08:27:15 -0400

    Added 'arbitrary' methods to interface

--- Deque.java
+++ Deque.java
@@ -4,13 +4,25 @@ public interface Deque<T> {
 	
     public void addFirst(T value);//Adds T value to the beginning of Queue
 
-    public T removeFirst();//Removes first element from beginning of Queue, returns removed value
+    public void offerFirst(T value);//Adds T value to the beginning of Queue
+
+    public T removeFirst();//Removes first element from beginning of Queue, returns removed value.  If empty, throws exception
+
+    public T pollFirst();//Removes first element from beginning of Queue, returns removed value.  If empty, returns null
 
     public T getFirst();//Returns first element without removing
+   
+    public T peekFirst();//Returns first element without removing
 
     public void addLast(T value);//Adds T value to the end of Queue
 
-    public T removeLast();//Removes last element from end of Queue, returns removed value
+    public void offerLast(T value);//adds T value to the end of Queue
+
+    public T removeLast();//Removes last element from end of Queue, returns removed value. If empty, throws exception
+
+    public T pollLast();//Removes last element from end of Queue, returns removed value.  If empty, returns null
 
     public T getLast();//Returns last element without removing
+
+    public T peekLast();//Returns last element without removing
 }
